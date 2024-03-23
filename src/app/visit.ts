f00316d2994f403e8981577b50e2a14ee244e2f1
martinoscorima <a20172445@pucp.edu.pe>
2024-03-23 12:37:51 -0500

    saff

--- src/app/visit.ts
+++ src/app/visit.ts
@@ -8,6 +8,7 @@ export class Visit {
     public date_entrance: string,
     public hour_entrance: string,
     public obs: string,
+    public house_address?: string,
     public visits?: number,
   ) { }
 
