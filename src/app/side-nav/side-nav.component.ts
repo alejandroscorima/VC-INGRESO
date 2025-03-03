d5f990bc5254adab2b0d9713deaacaa08985d21b
luisoscorima <astroscorima@gmail.com>
2025-03-03 02:41:55 -0500

    config solo para adm

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../app.component';
+import { CookiesService } from '../cookies.service';
+import { User } from '../user';
+
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.css']
 })
-export class SideNavComponent extends AppComponent{
+export class SideNavComponent extends AppComponent implements OnInit {
 
 }
